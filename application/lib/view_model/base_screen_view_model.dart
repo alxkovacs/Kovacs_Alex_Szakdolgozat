e278437d764b2b3ba1d32e75d53de2bb43330a5d
alxkovacs <alexkovacs.code@gmail.com>
2023-12-03 17:56:31 +0100

    beallitasok olda, kijelentkezesi logika

--- application/lib/view_model/base_screen_view_model.dart
+++ application/lib/view_model/base_screen_view_model.dart
@@ -23,6 +23,11 @@ class BaseScreenViewModel extends BaseViewModel {
     }
   }
 
+  void resetSelectedIndex() {
+    _selectedIndex = 0; // Visszaállítja az alapértelmezett értékre
+    notifyListeners(); // Frissíti a UI-t
+  }
+
   void _openBottomSheet(BuildContext context) {
     showModalBottomSheet(
       context: context,
