30b63c9721f2a77c2c14ca685023a8edad318160
alxkovacs <alexkovacs.code@gmail.com>
2023-12-07 19:58:24 +0100

    bejelentkezes javitasok, sizedbox, mvvm logika

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -29,7 +29,7 @@ void main() async {
   ));
   runApp(
     DevicePreview(
-      enabled: false,
+      enabled: true,
       builder: (context) => MultiProvider(
         providers: [
           ChangeNotifierProvider(create: (_) => SignUpScreenViewModel()),
