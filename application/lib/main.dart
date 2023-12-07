597463e393f8eb5d74b362df9b569ba818605a03
alxkovacs <alexkovacs.code@gmail.com>
2023-12-07 10:56:51 +0100

    javitasok, service, reszponziv, kommentek

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -27,7 +27,7 @@ void main() async {
   ));
   runApp(
     DevicePreview(
-      enabled: true,
+      enabled: false,
       builder: (context) => MultiProvider(
         providers: [
           ChangeNotifierProvider(create: (_) => SignUpScreenViewModel()),
