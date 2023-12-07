54e0d3de100b97863903eff30fa50bbf48f1a218
alxkovacs <alexkovacs.code@gmail.com>
2023-12-07 10:56:51 +0100

    javitasok, service, reszponziv, kommentek

--- application/lib/providers/login_view_model_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:application/view_model/login_screen_view_model.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final loginViewModelProvider = ChangeNotifierProvider((ref) {
-  return LoginViewModel();
-});
