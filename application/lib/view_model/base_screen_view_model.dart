b17605f190a13e396d170d8294154756afc02509
alxkovacs <alexkovacs.code@gmail.com>
2023-12-08 11:39:00 +0100

    customScaffold

--- application/lib/view_model/base_screen_view_model.dart
+++ application/lib/view_model/base_screen_view_model.dart
@@ -1,11 +1,8 @@
-import 'package:application/utils/colors.dart';
-import 'package:application/utils/styles/styles.dart';
-import 'package:application/utils/translation_en.dart';
-import 'package:application/view/screens/add_product_screen.dart';
 import 'package:application/view/screens/home_screen.dart';
 import 'package:application/view/screens/offers_screen.dart';
 import 'package:application/view/screens/products_screen.dart';
 import 'package:application/view/screens/settings_screen.dart';
+import 'package:application/view/widgets/custom_bottom_sheet_scaffold.dart';
 import 'package:flutter/material.dart';
 
 class BaseScreenViewModel extends ChangeNotifier {
@@ -34,25 +31,7 @@ class BaseScreenViewModel extends ChangeNotifier {
       context: context,
       isScrollControlled: true,
       builder: (BuildContext context) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 35.0),
-          child: Scaffold(
-            appBar: AppBar(
-              scrolledUnderElevation: 0,
-              centerTitle: true,
-              title: const Text(
-                TranslationEN.addProduct,
-                style: Styles.screenCenterTitle,
-              ),
-              leading: IconButton(
-                icon: const Icon(Icons.close),
-                onPressed: () => Navigator.pop(context),
-              ),
-            ),
-            backgroundColor: AppColor.lightBackgroundColor,
-            body: const AddProductScreen(),
-          ),
-        );
+        return const CustomBottomSheetScaffold();
       },
     );
   }
