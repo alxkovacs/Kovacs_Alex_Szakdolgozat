d7e354294b2ceeadd3979e4019d3c89a0e872301
alxkovacs <alexkovacs.code@gmail.com>
2023-12-08 19:10:53 +0100

    termek hozzaadas kod atszervezes, storedto, storesearchscreen

--- application/lib/providers/store_search_view_model_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:application/view_model/store_search_screen_view_model.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final storeSearchViewModelProvider =
-    ChangeNotifierProvider((ref) => StoreSearchScreenViewModel());
