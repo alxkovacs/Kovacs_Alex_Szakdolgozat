5ca4f240a70d9ffe338d4bf73600dbaac3220bab
alxkovacs <alexkovacs.code@gmail.com>
2023-12-10 00:20:22 +0100

    ajanlat dto, kod atszervezes, service

--- application/lib/providers/offer_view_model_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:application/view_model/offer_screen_view_model.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final offerViewModelProvider =
-    ChangeNotifierProvider<OfferScreenViewModel>((ref) {
-  return OfferScreenViewModel();
-});
