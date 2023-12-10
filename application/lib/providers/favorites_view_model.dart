a131df6ca676bbf840b3ee246c776ea050d4ccd1
alxkovacs <alexkovacs.code@gmail.com>
2023-12-10 04:02:56 +0100

    kedvencek kod atszervezes, dto, service, kommentek

--- application/lib/providers/favorites_view_model.dart
+++
@@ -1,7 +0,0 @@
-import 'package:application/view_model/favorites_screen_view_model.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final favoritesViewModelProvider =
-    ChangeNotifierProvider<FavoritesScreenViewModel>((ref) {
-  return FavoritesScreenViewModel();
-});
