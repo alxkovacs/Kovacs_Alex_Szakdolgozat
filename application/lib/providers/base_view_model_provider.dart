b02290e0d1133613e864cce48446e064e56fc0b8
alxkovacs <alexkovacs.code@gmail.com>
2023-12-11 01:19:31 +0100

    termek kod atszervezes, service, arak megjelenitese

--- application/lib/providers/base_view_model_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:application/view_model/base_screen_view_model.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final baseViewModelProvider = ChangeNotifierProvider((ref) {
-  return BaseScreenViewModel();
-});
--- application/lib/providers/sign_up_view_model_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:application/view_model/sign_up_view_model.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final signUpViewModelProvider = ChangeNotifierProvider((ref) {
-  return SignUpViewModel();
-});
